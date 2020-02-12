4c2889310af98fe5d0bd2a6409240fb0fdb6da31
sigilai <briansigilai@gmail.com>
2020-02-13 00:07:15 +0300

    Updates

---
+++ precache-manifest.ebf2f5c4479e863c92520821d52e699e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10dc0946b759a64a73673b478d0f3760",
+    "url": "/React-Quotes/index.html"
+  },
+  {
+    "revision": "079da41966313ef41555",
+    "url": "/React-Quotes/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e2e01c3535ca89cdc455",
+    "url": "/React-Quotes/static/js/2.da3eb8e8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/React-Quotes/static/js/2.da3eb8e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "079da41966313ef41555",
+    "url": "/React-Quotes/static/js/main.af6e3d46.chunk.js"
+  },
+  {
+    "revision": "726fd1d015738f77f78e",
+    "url": "/React-Quotes/static/js/runtime-main.23571f6a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-Quotes/static/media/logo.5d5d9eef.svg"
+  }
+]);
