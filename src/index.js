03ae1ec5d5fa339ad8cf29bacaaa1ce9335fb169
sigilai <briansigilai@gmail.com>
2020-02-13 10:40:50 +0300

    added Quote Modal

--- src/index.js
+++ src/index.js
@@ -5,11 +5,33 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
 import * as serviceWorker from './serviceWorker';
-import {MDBBtn,MDBContainer,MDBRow,MDBCol,MDBIcon} from "mdbreact";
+import {MDBBtn, MDBInput, MDBModal, MDBModalBody, MDBModalHeader, MDBModalFooter, MDBIcon, MDBBadge, MDBContainer, MDBRow, MDBCol} from "mdbreact";
 import {render} from "@testing-library/react";
 
 
+
+
+
+
 class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            modal:false,
+            quotes:[
+                {id:1,your_name: "Alex James", quote: "Give all you got!", author:"Jayme",up:0,down:0},
+                {id:2,your_name: "Alex James", quote: "Give all you got!", author:"Jayme",up:0,down:0},
+            ]
+        };
+    }
+
+    toggleModal = () => {
+        this.setState({
+           modal: !this.state.modal
+        });
+    };
+
+
     render(){
         return(
             <React.Fragment>
@@ -19,18 +41,91 @@ class App extends Component {
                     <MDBRow>
 
                         <MDBCol md="3">
-                            <MDBBtn color="primary">
+                            <MDBBtn color="primary" onClick={this.toggleModal}>
                                 <MDBIcon icon="magic" className="mr-1" /> Add Quote
                             </MDBBtn>
                         </MDBCol>
 
                         <MDBCol md="9">
-                            right
+                            <h3 className="text-uppercase my-3">Quotes</h3>
+                            {this.state.quotes.map(quote => (
+                                <Quote
+                                    key={quote.id}
+                                    id={quote.id}
+                                    your_name={quote.your_name}
+                                    quote={quote.quote}
+                                    author={quote.author}
+                                    upvotes={quote.up}
+                                    downvotes={quote.down}
+                                    onDelete=""
+                                />
+
+
+                            ))};
+
+
                         </MDBCol>
 
                     </MDBRow>
 
                 </MDBContainer>
+
+                <MDBModal isOpen={this.state.modal} toggle={this.toggleModal}>
+                    <MDBModalHeader
+                        className="text-center"
+                        titleClass="w-100 font-weight-bold"
+                        toggle={this.toggleModal}
+                    >
+                        New Quote
+                    </MDBModalHeader>
+                    <MDBModalBody>
+                        <form className="mx-3 grey-text">
+                            <MDBInput
+                                name="time"
+                                label="Time"
+                                icon="clock"
+                                hint="12:30"
+                                group type="text"
+
+                            />
+
+                            <MDBInput
+                                name="title"
+                                label="Title"
+                                icon="edit"
+                                hint="Briefing"
+                                group type="text"
+
+                            />
+
+                            <MDBInput
+                                name="location"
+                                label="Location (optional)"
+                                icon="map"
+                                group
+                                type="text"
+
+                            />
+
+                            <MDBInput
+                                name="description"
+                                label="Description (optional)"
+                                icon="sticky-note"
+                                group
+                                type="textarea"
+
+                            />
+
+                        </form>
+                    </MDBModalBody>
+                    <MDBModalFooter className="justify-content-center">
+                        <MDBBtn >
+                            Add
+                        </MDBBtn>
+
+                    </MDBModalFooter>
+                </MDBModal>
+
 
             </React.Fragment>
         )
@@ -42,9 +137,20 @@ class Quote extends Component{
 
     render() {
         return (
-            <div>
-                <p>Jobs</p>
-            </div>
+            <React.Fragment>
+                <h4>{this.props.your_name}</h4>
+                <h6>{this.props.quote}</h6>
+                <i>{this.props.author}</i>
+
+                <MDBRow>
+
+                    <MDBCol xs="6"><MDBCol xs="6"><MDBIcon color="primary" icon="thumbs-up" /></MDBCol></MDBCol>
+                    <MDBCol xs="6"><MDBIcon color="primary" icon="thumbs-down" /></MDBCol>
+                </MDBRow>
+
+
+
+            </React.Fragment>
         )
     }
 }
